f17dbc4d99a5f4984b02d09c6aca8654d15b5ffb
prayagshah07@gmail.com <34435248+prayagnshah@users.noreply.github.com>
2023-06-22 10:20:07 -0300

    WIP to get categories of times based on sunset and sunrise times

--- sample-data-generator.py
+++ sample-data-generator.py
@@ -1,10 +1,12 @@
 import pandas as pd
 from datetime import datetime, timedelta
 from suncalc import get_times
+from astral import LocationInfo
+from astral.sun import sun
 
 # Mentioning the start and end date
-start_date = pd.to_datetime("2021-07-05")
-end_date = pd.to_datetime("2021-07-31")
+start_date = pd.to_datetime("2021-07-05", utc=True)
+end_date = pd.to_datetime("2021-07-31", utc=True)
 
 # coords = pd.read_csv("GPS_log.csv", skiprows=1)
 latitude = 44.72
@@ -12,7 +14,7 @@ longitude = -62.80
 
 # Create an empty list to store the results of the sunset times
 result = []
-date_range = pd.date_range(start_date, end_date, freq="D", tz="America/Halifax")
+date_range = pd.date_range(start_date, end_date, freq="D", tz="UTC")
 
 # Loop through the dates and get the sunrise and sunset times
 for date in date_range:
@@ -60,6 +62,8 @@ for date in date_range:
 
 
 result_df = pd.DataFrame(result)
+
+# print(result_df)
 
 
 #
@@ -133,8 +137,90 @@ for date, res in zip(date_range, result):
     final_result.append(boss_table_df)
 
 combined_df = pd.concat(final_result, ignore_index=True)
-print(combined_df)
+
+# Calculate Julian day
+combined_df["JDay"] = pd.DatetimeIndex(combined_df["date_time"]).dayofyear
 
 
-# Create a list of dates between the start and end date
+combined_df = combined_df[["Filename", "Site", "date_time", "NewDate", "JDay"]]
+
+
+# Calling a function to create a list of datetimes
 datetime_range(start_date, end_date, timedelta(seconds=3600))
+
+data = pd.read_csv("sample-data.csv")
+df = pd.DataFrame(data)
+
+df["NewDate"] = pd.to_datetime(df["NewDate"], format="%Y%m%d_%H%M%S")
+
+# Calculate sunrise and sunset times
+location = LocationInfo(latitude=latitude, longitude=longitude)
+df["sunrise"] = df["NewDate"].apply(
+    lambda x: sun(location.observer, date=x.date())["sunrise"]
+)
+df["sunset"] = df["NewDate"].apply(
+    lambda x: sun(location.observer, date=x.date())["sunset"]
+)
+
+early_start = pd.Timestamp("2021-07-05")
+early_end = pd.Timestamp("2021-07-20")
+late_start = pd.Timestamp("2021-07-25")
+late_end = pd.Timestamp("2021-07-31")
+
+
+# Define time categories based on conditions
+def assign_time_category(row):
+    sunrise = row["sunrise"]
+    sunset = row["sunset"]
+    current_time = row["NewDate"]
+
+    if early_start <= current_time <= early_end:
+        if current_time >= sunrise - timedelta(
+            seconds=3900
+        ) and current_time <= sunrise + timedelta(seconds=2940):
+            return "Early Deployment - Early AM"
+        elif current_time > sunrise + timedelta(
+            seconds=3000
+        ) and current_time <= sunrise + timedelta(seconds=9000):
+            return "Early Deployment - Mid AM"
+        elif current_time > sunrise + timedelta(
+            seconds=9036
+        ) and current_time <= sunrise + timedelta(seconds=18000):
+            return "Early Deployment - Late AM"
+    elif late_start <= current_time <= late_end:
+        if current_time >= sunrise - timedelta(
+            seconds=3900
+        ) and current_time <= sunrise + timedelta(seconds=2940):
+            return "Late Deployment - Early AM"
+        elif current_time > sunrise + timedelta(
+            seconds=3000
+        ) and current_time <= sunrise + timedelta(seconds=9000):
+            return "Late Deployment - Mid AM"
+        elif current_time > sunrise + timedelta(
+            seconds=9036
+        ) and current_time <= sunrise + timedelta(seconds=18000):
+            return "Late Deployment - Late AM"
+    else:
+        if current_time >= sunset - timedelta(
+            seconds=4200
+        ) and current_time <= sunset + timedelta(seconds=600):
+            return "Dusk"
+        elif current_time >= sunset - timedelta(
+            seconds=19800
+        ) and current_time < sunset - timedelta(seconds=4500):
+            return "Daytime"
+        else:
+            return "Nocturnal"
+
+
+# Assign time categories to each row
+df["TimeCategory"] = df.apply(assign_time_category, axis=1)
+
+# Get random samples for each category
+sample_size = 1  # Change this value to the desired number of samples per category
+random_samples = df.groupby("TimeCategory").apply(lambda x: x.sample(sample_size))
+
+# Reset index
+random_samples.reset_index(drop=True, inplace=True)
+
+print(random_samples)
